f05fe35c0c6e606931d803d5d42266a4441e955e
gamma7597 <louis.braud@hotmail.fr>
2022-04-19 06:29:12 -0400

    commit

--- EHS_Audit/Audit_back/server.js
+++ EHS_Audit/Audit_back/server.js
@@ -101,7 +101,7 @@ require("./app/routes/rules/tdt_rule.routes")(app);
 require("./app/routes/file.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   logger.debug(`Server is running on port ${PORT}.`);
 });
